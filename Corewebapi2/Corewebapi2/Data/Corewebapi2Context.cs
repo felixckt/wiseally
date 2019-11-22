3e2e339ff7c395475563fa67b8eb628caa12284d
Felix Cheung <felix.ckt@gmail.com>
2019-11-22 18:38:33 +0800

    1
    1

---
+++ Corewebapi2/Corewebapi2/Data/Corewebapi2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Corewebapi2.Model;
+
+namespace Corewebapi2.Models
+{
+    public class Corewebapi2Context : DbContext
+    {
+        public Corewebapi2Context (DbContextOptions<Corewebapi2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Corewebapi2.Model.Order> Order { get; set; }
+    }
+}
---
+++ TelerikAspNetCoreAppw2/TelerikAspNetCoreAppw2/Data/TelerikAspNetCoreAppw2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TelerikAspNetCoreAppw2.Models
+{
+    public class TelerikAspNetCoreAppw2Context : DbContext
+    {
+        public TelerikAspNetCoreAppw2Context (DbContextOptions<TelerikAspNetCoreAppw2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TelerikAspNetCoreAppw2.Models.Order> Order { get; set; }
+    }
+}
